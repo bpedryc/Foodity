327714c3b4f7f0ff779d7d490314efbdd4b7004c
Bartek Pedryc <b.pedryc11@gmail.com>
2021-04-19 00:48:35 +0200

    Add interface for creating new categories

--- Foodity/app/src/main/java/com/haxos/foodity/ui/main/notes/NotesFragment.kt
+++ Foodity/app/src/main/java/com/haxos/foodity/ui/main/notes/NotesFragment.kt
@@ -2,16 +2,18 @@ package com.haxos.foodity.ui.main.notes
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.haxos.foodity.R
 import com.haxos.foodity.data.model.Note
 import com.haxos.foodity.databinding.FragmentNotesBinding
 import com.haxos.foodity.ui.main.SearchResultAdapter
@@ -35,8 +37,19 @@ class NotesFragment : Fragment() {
         binding = FragmentNotesBinding.inflate(inflater)
 
         val toolbar: Toolbar = binding.toolbarActivityMain
+        toolbar.inflateMenu(R.menu.menu_notes_categories)
         toolbar.setNavigationOnClickListener {
             startActivity(Intent(activity, SettingsActivity::class.java))
+        }
+        toolbar.setOnMenuItemClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(requireActivity())
+            builder.setView(R.layout.dialog_note_category)
+                .setTitle("New category")
+                .setMessage("Create a category")
+                .setPositiveButton(android.R.string.yes) {_, _ -> }
+                .setNegativeButton(android.R.string.no) {_, _ -> }
+                .show()
+            true
         }
 
         val searchView = binding.notesSearchView
@@ -76,4 +89,6 @@ class NotesFragment : Fragment() {
             replace(NoteContentFragment.newInstance(note.id))
         }
     }
+
+
 }
