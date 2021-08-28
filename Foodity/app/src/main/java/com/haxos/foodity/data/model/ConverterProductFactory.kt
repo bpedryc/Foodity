2df11daa39d6b305dc1ffc9bafaa76b3d94f2b39
Bartek Pedryc <b.pedryc11@gmail.com>
2021-08-28 23:49:36 +0200

    Add more products for weight converter

--- Foodity/app/src/main/java/com/haxos/foodity/data/model/ConverterProductFactory.kt
+++ Foodity/app/src/main/java/com/haxos/foodity/data/model/ConverterProductFactory.kt
@@ -6,7 +6,15 @@ class ConverterProductFactory {
     companion object Factory {
         val products: List<ConverterProduct> = listOf(
             ConverterProduct(R.string.product_white_long_rice, 0.74),
-            ConverterProduct(R.string.product_white_short_rice, 0.8)
+            ConverterProduct(R.string.product_white_short_rice, 0.8),
+            ConverterProduct(R.string.product_sugar, 0.85),
+            ConverterProduct(R.string.product_butter, 0.955),
+            ConverterProduct(R.string.product_almonds, 0.613),
+            ConverterProduct(R.string.product_canola_oil, 0.909),
+            ConverterProduct(R.string.product_olive_oil, 0.9),
+            ConverterProduct(R.string.product_soy_sauce, 1.12),
+            ConverterProduct(R.string.product_honey, 1.44),
+            ConverterProduct(R.string.product_maple_syrup, 1.01)
         )
     }
 }
