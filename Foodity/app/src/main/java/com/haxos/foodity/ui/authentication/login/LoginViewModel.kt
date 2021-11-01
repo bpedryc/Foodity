b8d90d1b02d55350d990e0947fd005ef0ce48c9c
bpedryc <b.pedryc11@gmail.com>
2021-11-01 17:43:14 +0100

    5 characters for password fix

--- Foodity/app/src/main/java/com/haxos/foodity/ui/authentication/login/LoginViewModel.kt
+++ Foodity/app/src/main/java/com/haxos/foodity/ui/authentication/login/LoginViewModel.kt
@@ -40,8 +40,6 @@ class LoginViewModel @Inject constructor (
     fun loginDataChanged(username: String, password: String) {
         if (!isUserNameValid(username)) {
             _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
-        } else if (!isPasswordValid(password)) {
-            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
         } else {
             _loginForm.value = LoginFormState(isDataValid = true)
         }
@@ -53,10 +51,6 @@ class LoginViewModel @Inject constructor (
         } else {
             username.isNotBlank()
         }
-    }
-
-    private fun isPasswordValid(password: String): Boolean {
-        return password.length > 5
     }
 
     fun logout(accountManagerActivity: Activity) {
